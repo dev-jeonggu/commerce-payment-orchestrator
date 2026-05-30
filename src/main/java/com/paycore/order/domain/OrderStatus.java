8001ca96365b6c9907f09a3d5409a498b3e2e188
Kim JeongHyeon <dev.jeonggu@gmail.com>
2026-05-30 12:26:07 +0900

    refactor: remove ecommerce domain (Order, Inventory, Point, Item)

--- src/main/java/com/paycore/order/domain/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.paycore.order.domain;
-
-public enum OrderStatus {
-    PENDING,            // 주문 생성됨, 결제 대기
-    PENDING_PAYMENT,    // 가상계좌 발급됨, 입금 대기
-    PAID,               // 결제 완료
-    CANCELLED,          // 취소됨
-    FAILED              // 결제 실패
-}
